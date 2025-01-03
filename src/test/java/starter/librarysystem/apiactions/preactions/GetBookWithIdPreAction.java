4f6b7190379cb1568597875b3f5e5bc6be5d0133
Claretes Nishoban <106062267+Clare06@users.noreply.github.com>
2025-01-03 17:52:40 +0530

    added before all

--- src/test/java/starter/librarysystem/apiactions/preactions/GetBookWithIdPreAction.java
+++ src/test/java/starter/librarysystem/apiactions/preactions/GetBookWithIdPreAction.java
@@ -1,21 +1,20 @@
 package starter.librarysystem.apiactions.preactions;
 
-import io.cucumber.java.Before;
 import io.cucumber.java.BeforeAll;
-import io.cucumber.java.en.Given;
-import net.serenitybdd.core.steps.UIInteractions;
+
+import org.junit.jupiter.api.Tag;
 import starter.librarysystem.api.ApiUtil;
 import starter.librarysystem.dto.Book;
+
 
 public class GetBookWithIdPreAction {
     public static Book globalBookToTest;
 
-//    @Given("Given that book is available in library with title {string} and author {string}")
-    @Before("@searchByIdAsUser")
+    @BeforeAll
     public static void givenThatBookIsAvailableInLibraryWithTitleAndAuthor() {
+
         Book book = new Book("Jenkins Tutorial", "Reyon");
         ApiUtil util = new ApiUtil();
-        globalBookToTest= util.asUser().addBook(book);
+        globalBookToTest = util.asUser().addBook(book);
     }
-
 }
--- src/test/resources/features/api/fetchbook/getbookwithidasuser.feature
+++ src/test/resources/features/api/fetchbook/getbookwithidasuser.feature
@@ -1,6 +1,6 @@
 Feature: Search book in library system as User role with book id
 @searchByIdAsUser
   Scenario: Searching book  in library as an user role
-#    Given Given that book is available in library with title "Jenkins Tutorial" and author "Reyon"
+
     When As an user fetch the book with its id.
     Then He should receive the book with title "Jenkins Tutorial"
